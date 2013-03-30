d606e805b7e146c5266eb584ee1abd591c4019e4
Takahiro Sato <ph1.ph2.sa25@gmail.com>
2013-03-30 11:57:23 +0900

    result 画面作成

--- web.rb
+++ web.rb
@@ -3,12 +3,21 @@ require 'erb'
 
 require 'sinatra/reloader' if development?
 
+# TOP
 get '/' do
   erb :index
 end
 
-get '/c/1/q/:id' do
-  @id = params[:id]
+# quiz
+get '/c/:course_id/q/:quiz_id' do
+  @quiz_id = params[:quiz_id]
   erb :quiz
+end
+
+# result 
+get '/c/:course_id/result/put/:name' do
+  @course_id = params[:course_id]
+  @name= params[:name]
+  erb :result
 end
 
