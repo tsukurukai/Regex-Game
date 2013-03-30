7f5b0158da31ffb91246b179124c1369584531fb
Takahiro Sato <ph1.ph2.sa25@gmail.com>
2013-03-30 12:09:53 +0900

    add result画面

--- web.rb
+++ web.rb
@@ -9,15 +9,32 @@ get '/' do
 end
 
 # quiz
+# render quiz 
 get '/c/:course_id/q/:quiz_id' do
+  @course_id = params[:course_id]
+  @quiz_id   = params[:quiz_id]
+  erb :quiz
+end
+
+# quiz
+# answer check
+post '/c/:course_id/q/:quiz_id/answer' do
+  @course_id = params[:course_id]
   @quiz_id = params[:quiz_id]
   erb :quiz
 end
 
 # result 
-get '/c/:course_id/result/put/:name' do
+post '/c/:course_id/result' do
   @course_id = params[:course_id]
-  @name= params[:name]
+  erb :result
+end
+
+# result 
+# put answerer's name
+post '/c/:course_id/result/put/:name' do
+  @course_id = params[:course_id]
+  @name      = params[:name]
   erb :result
 end
 
