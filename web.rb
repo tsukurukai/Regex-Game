643f392d7389a0bcefb88409360e0dff8d60bdb7
bati11 <mail.bati11@gmail.com>
2013-03-30 11:35:30 +0900

    TOPページ追加

--- web.rb
+++ web.rb
@@ -3,8 +3,12 @@ require 'erb'
 
 require 'sinatra/reloader' if development?
 
-get '/q/:id' do
-  @id = params[:id]
+get '/' do
   erb :index
+end
+
+get '/c/1/q/:id' do
+  @id = params[:id]
+  erb :quiz
 end
 
