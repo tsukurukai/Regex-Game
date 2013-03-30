4ab3178e73465d96f769c54347b516bb4cf45d9a
hanhan <hanhan@hanhans-Mac.local>
2013-03-30 16:43:14 +0900

    緊急修正

--- data/insert_quiz.sh
+++ data/insert_quiz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongo regex --eval "db.dropDatabase()"
+mongo regex --eval "printjson(db.study.drop())"
 ruby yml2json.rb
 mongoimport -d regex -c study -type json -file study.js -headerline
 rm study.js
