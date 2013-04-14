6aee73dc73576841d6cde258bd5712cb84c93f1d
bati11 <mail.bati11@gmail.com>
2013-04-14 23:20:28 +0900

    インデントを修正

--- model.rb
+++ model.rb
@@ -2,89 +2,88 @@ require 'mongo'
 require 'json'
 
 class BaseModel
-    def initialize()
-        if 'production' != ENV['RACK_ENV'] 
-            connection = Mongo::Connection.new('localhost')
-        else
-            connection = Mongo::Connection.new('alex.mongohq.com', 10015)
-        end
-        @db = connection.db('app14201811')
-        if 'production' == ENV['RACK_ENV'] 
-            @db.authenticate('regex', 'regex')
-		end
+  def initialize()
+    if 'production' != ENV['RACK_ENV'] 
+      connection = Mongo::Connection.new('localhost')
+    else
+      connection = Mongo::Connection.new('alex.mongohq.com', 10015)
     end
+    @db = connection.db('app14201811')
+    if 'production' == ENV['RACK_ENV'] 
+      @db.authenticate('regex', 'regex')
+    end
+  end
 end
 
 class RegexModel < BaseModel
 
-    def initialize()
-        super()
-        @coll = @db.collection('study')
-    end
+  def initialize()
+    super()
+    @coll = @db.collection('study')
+  end
 
-    def getCourses()
-        res = Array.new
-        @coll.find_one['courses'].each{|co|
-            h = Hash.new
-            h['name'] = co['name']
-            h['id'] = co['id']
-            res.push(h)
-        }
-        return res
-    end
+  def getCourses()
+    res = Array.new
+    @coll.find_one['courses'].each{|co|
+      h = Hash.new
+      h['name'] = co['name']
+      h['id'] = co['id']
+      res.push(h)
+    }
+    return res
+  end
 
-    def getQuiz(course_id, quiz_id)
-        quizes = Array.new
-        @coll.find_one['courses'].each{ | co |
-            if ( co['id'] == course_id)
-                quizes = co['quizes'] 
-                break;
-            end    
-        }
-        h = Hash.new
-        h['count'] = quizes.length
-        h['quiz'] = quizes[quiz_id.to_i == 0 || quiz_id.to_i > quizes.length ? 0 : quiz_id.to_i - 1]
-        return h 
-    end
+  def getQuiz(course_id, quiz_id)
+    quizes = Array.new
+    @coll.find_one['courses'].each{ | co |
+      if ( co['id'] == course_id)
+        quizes = co['quizes']
+        break;
+      end
+    }
+    h = Hash.new
+    h['count'] = quizes.length
+    h['quiz'] = quizes[quiz_id.to_i == 0 || quiz_id.to_i > quizes.length ? 0 : quiz_id.to_i - 1]
+    return h
+  end
 
 end
 
 
 class RankingModel < BaseModel
-    def initialize()
-        super()
-        @coll = @db.collection('ranking')
-    end
+  def initialize()
+    super()
+    @coll = @db.collection('ranking')
+  end
 
-    def insert(name, time)
-        h = Hash.new
-        h['name'] = name
-        h['time'] = time.to_i
-        @coll.insert(h)
-    end
+  def insert(name, time)
+    h = Hash.new
+    h['name'] = name
+    h['time'] = time.to_i
+    @coll.insert(h)
+  end
 
-    def getList(page=1, limit=5)
-        i=1;
-        list = Array.new
-        @coll.find().sort(:time).each do |slice|
-            if i > (page -1) * limit && i <= (page) * limit 
-                list.push(slice)
-            end
-            i += 1
-        end
-        return list
+  def getList(page=1, limit=5)
+    i=1;
+    list = Array.new
+    @coll.find().sort(:time).each do |slice|
+      if i > (page -1) * limit && i <= (page) * limit 
+        list.push(slice)
+      end
+      i += 1
     end
+    return list
+  end
 
-    def find(username)
-        i=1;
-        @coll.find().sort(:time).each do |slice|
-            if slice['name'] == username
-                return i;
-            end
-            i +=1
-        end
-        return nil
+  def find(username)
+    i=1;
+    @coll.find().sort(:time).each do |slice|
+      if slice['name'] == username
+        return i;
+      end
+      i +=1
     end
+    return nil
+  end
 end
 
- 
