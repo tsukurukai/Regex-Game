b083961b0f9ae8c50b6550653852c4c5c5d2400d
hanhan1978 <ryo.tomidokoro@gmail.com>
2013-08-16 22:34:29 +0900

    管理系追加

---
+++ web_admin.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+class Admin < Sinatra::Base
+
+    if development?
+        require 'sinatra/reloader'
+        register Sinatra::Reloader
+    end
+
+    get '/admin/' do
+        admin_erb :'admin/index' 
+    end
+
+    get '/admin/course/list' do
+        @results = Course.all 
+        admin_erb :"admin/course/list" 
+    end
+
+    get '/admin/course/detail/:course_id' do
+        @course_id = params[:course_id]
+        @course = Course.find_by_id(@course_id.to_i)
+        quiz = @course.quiz(1)
+        admin_erb :"admin/course/detail" 
+    end
+
+    def admin_erb(template, options={}) 
+        erb(template, options.merge(:layout => :'admin/layout')) 
+    end
+end
+
+use Admin;
