ce33f6ecadf4a1f620656f332dc2b4fe9858522b
bati11 <mail.bati11@gmail.com>
2014-03-29 17:47:32 +0900

    Quizのテストを追加

---
+++ spec/app/quiz_spec.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe Quiz do
+
+  before do
+    @sut = Quiz.new
+  end
+
+  describe '#push' do
+
+    it 'its items should have no item' do
+      @sut.items.should be_empty
+    end
+
+    it 'should have 1 items after called "push" 1 times' do
+      @sut.push("sentence" => "aaa123bbb", "target_start_index" => 3, "target_length" => 3)
+      @sut.items.should have(1).items
+    end
+
+    it 'should have 2 items after called "push" 2 times' do
+      @sut.push("sentence" => "aaa123bbb", "target_start_index" => 3, "target_length" => 3)
+      @sut.push("sentence" => "ABC5555DEF", "target_start_index" => 3, "target_length" => 4)
+      @sut.items.should have(2).items
+    end
+
+  end
+end
+
